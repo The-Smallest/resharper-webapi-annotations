17145e909c3e0271d6f15497dbba46c106e1ba01
Ivan <mutsmu@gmail.com>
2015-03-28 00:07:05 +0300

    Folders reorganized

---
+++ tools/build.cmd
@@ -0,0 +1,1 @@
+NuGet.exe pack ..\src\WebApi.Annotations.nuspec -Version %1 -Verbosity Detail -OutputDirectory ..\output
---
+++ tools/publish.cmd
@@ -0,0 +1,4 @@
+@echo Building version %1
+NuGet.exe pack ..\src\WebApi.Annotations.nuspec -Version %1 -Verbosity Detail -OutputDirectory ..\output
+@echo Pushing WebApi.Annotations.%1.nupkg
+NuGet.exe push ..\output\WebApi.Annotations.%1.nupkg %2 -Source https://resharper-plugins.jetbrains.com
