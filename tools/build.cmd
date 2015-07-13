915120794d7b7ba5b75848dc7d4dde1174bcdd4c
Ivan <mutsmu@gmail.com>
2015-07-13 21:04:57 +0300

    Updated to Resharper 9.1

--- tools/build.cmd
+++ tools/build.cmd
@@ -1,1 +1,3 @@
+if not exist ..\output mkdir ..\output
+
 NuGet.exe pack ..\src\WebApi.Annotations.nuspec -Version %1 -Verbosity Detail -OutputDirectory ..\output
--- tools/publish.cmd
+++ tools/publish.cmd
@@ -1,3 +1,5 @@
+if not exist ..\output mkdir ..\output
+
 @echo Building version %1
 NuGet.exe pack ..\src\WebApi.Annotations.nuspec -Version %1 -Verbosity Detail -OutputDirectory ..\output
 @echo Pushing WebApi.Annotations.%1.nupkg
